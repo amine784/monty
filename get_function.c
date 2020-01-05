3927e017df0050a7e6c6c301d1acd50996661797
amine784 <55138513+amine784@users.noreply.github.com>
2020-01-05 20:27:43 +0100

    update

--- get_function.c
+++ get_function.c
@@ -1,30 +1,23 @@
 #include "monty.h"
 /**
-* get_function-function that chek the correct operation.
-*@stack:the head of the stack
-*@nbre_line: numbre of the actual line of the opcode
-*@pcode:char type, the opcode to run
+* fn_helper-function that fetch the correct operation
+*@stack:the head of stack or queue
+*@pcd:functino to fetch
+*@nbre_line: in which line
 *Return:Always success
 **/
-void get_function(char *pcode, stack_t **stack, unsigned int nbre_line)
+int fn_helper(stack_t **stack, char *pcd, unsigne int nbre_line)
 {
-instruction_t operation[] = {
-{"push", _push},
-{"pall", _pall},
-{"pint", _pint},
-{"pop", _pop},
-{"swap", _swap},
-{"add", _add},
-{"nop", _nop},
-{NULL, NULL}
-};
+instruction_t operation[] = {{"pint", pint}, {"pall", pall}, {NULL, NULL}};
 int i = 0;
-for (; operation[i].opcode != NULL; i++)
-if (strcmp(operation[i].opcode, pcode) == 0)
+for (; operation[i].opcode; i++)
 {
-operation[i].f(stack, nbre_line);
-return;
+if (strcmp(operation[i].opcode, pcd) == 0)
+{
+(operation[i].f)(stack, nbre_line);
+exit(EXIT_SUCCESS);
 }
-printf("L%d: unknown instruction %s\n", nbre_line, pcode);
+}
+dprintf(STDERR_FILENO, "L%d: unknown instruction %s\n", nbre_line, pcd);
 exit(EXIT_FAILURE);
 }
