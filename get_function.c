1e54544adec5ee1d6094b2fa54e90b01115842f0
amine784 <55138513+amine784@users.noreply.github.com>
2020-01-08 20:17:37 +0100

    update

--- get_function.c
+++ get_function.c
@@ -10,7 +10,6 @@ int fn_helper(stack_t **stack, char *pcd, unsigne int nbre_line)
 {
 instruction_t operation[] = {{"pint", pint}, {"pall", pall}, {NULL, NULL}};
 int i = 0;
-  /**
 for (; operation[i].opcode; i++)
 {
 if (strcmp(operation[i].opcode, pcd) == 0)
@@ -22,4 +21,3 @@ exit(EXIT_SUCCESS);
 dprintf(STDERR_FILENO, "L%d: unknown instruction %s\n", nbre_line, pcd);
 exit(EXIT_FAILURE);
 }
-**/
